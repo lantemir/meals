c80dcd22f61ff4d5e0d2cfa4c20ff0d181e8925a
lantemir <temiros@mail.ru>
2026-02-05 16:16:44 +0500

    category meals

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetails extends StatelessWidget {
+  MealDetails({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: ,
+      ),
+    )
+  }  
+}
